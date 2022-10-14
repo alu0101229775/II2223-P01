133299c6803114354d734cde7d93c3301dcb07c2
Gerard CV <65600540+alu0101229775@users.noreply.github.com>
2022-10-14 04:25:27 +0100

    Print in console the Game Object's name and tag

---
+++ PrintObjectsScript.cs
@@ -0,0 +1,25 @@
+/**
+ * Print in console the Game Object's name and tag
+ * 
+ * @date 14/10/2022
+ * @author Gerard Antony Caramazza Vilá
+ * @email alu0101229775@ull.edu.es 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrintObjectsScript : MonoBehaviour {
+  // Start is called before the first frame update
+  void Start() {
+    GameObject go = GameObject.FindWithTag(tag);
+    Debug.Log("Game Object: " + go.name + "\nTag: " + tag);
+  }
+
+  // Update is called once per frame
+  void Update() {
+    
+  }
+}
